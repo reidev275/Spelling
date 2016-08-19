1a7cfa52b8df7ca87961ff1f99a658c3379b0dce
Reid Evans <reidev275@gmail.com>
2016-08-18 21:59:49 -0400

    break letter into a module

---
+++ Letter.elm
@@ -0,0 +1,101 @@
+module Letter exposing (Msg, Model, update, view, init)
+
+import Html exposing (..)
+import Html.App as App
+import Html.Attributes exposing (..)
+import Html.Events exposing (on)
+import Json.Decode as Json exposing ((:=))
+import Mouse exposing (Position)
+
+
+-- MODEL
+
+type alias Model =
+  { letter : String
+  , position : Position
+  , drag : Maybe Drag
+  }
+
+
+type alias Drag = 
+  { start : Position
+  , current : Position
+  }
+
+init : String -> Model
+init s =
+  Model s (Position 200 200) Nothing
+
+-- UPDATE
+
+type Msg
+  = DragStart Position
+  | DragAt Position
+  | DragEnd Position
+
+update : Msg -> Model -> Model
+update msg ({letter, position, drag} as model) =
+  case msg of
+    DragStart xy ->
+      Model letter position (Just (Drag xy xy))
+
+    DragAt xy ->
+      Model letter position (Maybe.map (\{start} -> Drag start xy) drag)
+
+    DragEnd _ ->
+      Model letter (getPosition model) Nothing
+
+
+-- SUBSCRIPTIONS
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  case model.drag of
+    Nothing -> 
+      Sub.none
+
+    Just _ ->
+      Sub.batch [ Mouse.moves DragAt, Mouse.ups DragEnd ]
+
+
+-- VIEW
+
+(=>) = (,)
+
+view : Model -> Html Msg
+view model =
+  let 
+    realPosition = getPosition model
+  in
+    div
+      [ onMouseDown
+      , style
+        [ "cursor" => "move"
+        , "position" => "absolute"
+        , "left" => px realPosition.x
+        , "top" => px realPosition.y
+        , "font-size" => "48px"
+        ]
+      ]
+      [ text model.letter
+      ]
+
+px : Int -> String
+px number =
+  toString number ++ "px"
+
+getPosition : Model -> Position
+getPosition {_, position, drag} =
+  case drag of
+    Nothing -> 
+      positioin
+
+    Just { start, current } ->
+      Position
+        (position.x + current.x - start.x)
+        (position.y + current.y - start.y)
+
+onMouseDown : Attribute Msg
+onMouseDown =
+  on "mousedown" (Json.map DragStart Mouse.position)
+
